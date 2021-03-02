1de90be04df2530b495640b4655c20a6db891bf8
HZY <hzy@192.168.2.26>
2021-03-02 15:41:20 +0800

    test

---
+++ src/main/java/com/util/MyUtil.java
@@ -0,0 +1,20 @@
+package com.util;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.servlet.http.HttpSession;
+import com.po.Buser;
+public class MyUtil {
+
+	public static String getStringID(){
+		String id=null;
+		Date date=new Date();
+		SimpleDateFormat sdf=new SimpleDateFormat("yyyyMMddHHmmssSSS"); 
+		id=sdf.format(date);
+		return id;
+	}
+
+	public static Integer getUserId(HttpSession session) {
+		Buser ruser = (Buser)session.getAttribute("bruser");
+		return ruser.getId();
+	}
+}
